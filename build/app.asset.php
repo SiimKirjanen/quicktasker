8c30ef23b9c7b02d4eac12725b9244c14011da66
Siim Kirjanen <siim.kirjanen@pipedrive.com>
2024-10-23 14:48:35 +0300

    Made user page custom field values refresh on navigator refresh

--- build/app.asset.php
+++ build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '4112e1019c60b2a33786');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '0c9d0393cd4f58887d21');
