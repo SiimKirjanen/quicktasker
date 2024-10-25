113fa7e425cac3dea74e4e81a9dcfb72272a3e81
Siim Kirjanen <siim.kirjanen@pipedrive.com>
2024-10-25 16:06:40 +0300

    Version bump, changelog update

--- build/app.asset.php
+++ build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '4d9823c982f0859bc2f1');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'da1a8725aa6ee3280cac');
--- build/userApp.asset.php
+++ build/userApp.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'b4baa9c9be68bb24d5f0');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'bf053933375e61895d25');
