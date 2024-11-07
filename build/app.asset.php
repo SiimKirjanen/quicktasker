d338be194f6b6ed3379da2d10a645759ab00df8c
Siim Kirjanen <siim.kirjanen@pipedrive.com>
2024-11-07 16:00:29 +0200

    More text to help file

--- build/app.asset.php
+++ build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '59451469ca2be27b059b');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'f2e82f7890ac9fc6c92a');
--- build/userApp.asset.php
+++ build/userApp.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '793625baf73ee41e58a6');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '1d3ce41f40f9aa8e0e6a');
