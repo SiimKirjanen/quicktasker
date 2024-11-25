dfd2f25397031c0995424c1966e9b3e3b55b78e4
Siim Kirjanen <siim.kirjanen@pipedrive.com>
2024-11-25 22:31:26 +0200

    Added cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# List of files and directories to remove
+FILES_TO_REMOVE=(
+    "tsconfig.json"
+    ".eslintrc.js"
+    ".prettierrc.js"
+    ".vscode"
+    ".github"
+    ".git"
+    ".husky"
+    ".gitignore"
+    "package-lock.json"
+    "package.json"
+    "README.md"
+    "tailwind.config.js"
+    "webpack.config.js",
+    "node_modules"
+)
+
+# Prompt the user for confirmation
+read -p "Do you want to continue with the cleanup? (y/n): " CONFIRMATION
+
+if [[ "$CONFIRMATION" != "y" ]]; then
+  echo "Cleanup aborted."
+  exit 1
+fi
+
+# Remove each file and directory
+for FILE in "${FILES_TO_REMOVE[@]}"; do
+  if [ -e "$FILE" ]; then
+    rm -rf "$FILE"
+    echo "Removed $FILE"
+  else
+    echo "$FILE does not exist"
+  fi
+done
