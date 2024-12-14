d39e9d92e382058551d45f4671d6f044abc9cfb7
Siim Kirjanen <siim.kirjanen@pipedrive.com>
2024-12-14 15:26:19 +0200

    build

--- build/app.asset.php
+++ build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '5951c2b440e1c8a9626e');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'dfdf0a5680d52e788a81');
