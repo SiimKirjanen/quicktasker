f19b2ade087d43c023631f6f3a013f8a8e7507ed
Siim Kirjanen <siim.kirjanen@pipedrive.com>
2025-05-16 15:28:15 +0300

    Slack service added

---
+++ php/services/SlackService.php
@@ -0,0 +1,56 @@
+<?php
+
+
+namespace WPQT\Slack;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if ( ! class_exists( 'WPQT\Slack\SlackService' ) ) {
+    class SlackService {
+        /**
+         * Send a message to Slack using Incoming Webhooks
+         * 
+         * @param string $webhook_url The Slack webhook URL
+         * @param string $message The message to send
+         * @param array $additional_data Optional additional data like channel, username, etc.
+         * @return string The response body on success
+         * @throws \Exception If something goes wrong with the request
+         */
+        public function sendMessage($webhook_url, $message, $additional_data = []) {
+            if (empty($webhook_url)) {
+                throw new \Exception('Slack webhook URL is empty');
+            }
+            
+            $payload = [
+                'text' => $message,
+            ];
+            
+            $payload = array_merge($payload, $additional_data);
+            
+            $args = [
+                'body'        => wp_json_encode($payload),
+                'headers'     => ['Content-Type' => 'application/json'],
+                'timeout'     => 15,
+                'blocking'    => true,
+                'sslverify'   => true,
+            ];
+            
+            $response = wp_remote_post($webhook_url, $args);
+            
+            if (is_wp_error($response)) {
+                throw new \Exception('Slack notification error: ' . $response->get_error_message());
+            } 
+            
+            $code = wp_remote_retrieve_response_code($response);
+            if ($code !== 200) {
+                throw new \Exception('Slack API error: Received response code ' . $code);
+            }
+            
+            $body = wp_remote_retrieve_body($response);
+            
+            return $body;
+        }
+    }
+}
