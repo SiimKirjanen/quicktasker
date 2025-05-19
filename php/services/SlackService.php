2d786cf565f64ba490529c26c2574f5c6c1ff961
Siim Kirjanen <siim.kirjanen@pipedrive.com>
2025-05-19 17:51:27 +0300

    Wait for response configuration for Slack send message

--- php/services/SlackService.php
+++ php/services/SlackService.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace WPQT\Slack;
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -15,10 +14,11 @@ if ( ! class_exists( 'WPQT\Slack\SlackService' ) ) {
          * @param string $webhook_url The Slack webhook URL
          * @param string $message The message to send
          * @param array $additional_data Optional additional data like channel, username, etc.
-         * @return string The response body on success
+         * @param bool $wait_for_response Whether to wait for response from Slack (default: true)
+         * @return string|null The response body on success if waiting for response, null if not waitin
          * @throws \Exception If something goes wrong with the request
          */
-        public function sendMessage($webhook_url, $message, $additional_data = []) {
+        public function sendMessage($webhook_url, $message, $additional_data = [], $wait_for_response = true) {
             if (empty($webhook_url)) {
                 throw new \Exception('Slack webhook URL is empty');
             }
@@ -33,11 +33,15 @@ if ( ! class_exists( 'WPQT\Slack\SlackService' ) ) {
                 'body'        => wp_json_encode($payload),
                 'headers'     => ['Content-Type' => 'application/json'],
                 'timeout'     => 15,
-                'blocking'    => true,
+                'blocking'    => $wait_for_response,
                 'sslverify'   => true,
             ];
             
             $response = wp_remote_post($webhook_url, $args);
+
+            if (!$wait_for_response) {
+                return null;
+            }
             
             if (is_wp_error($response)) {
                 throw new \Exception('Slack notification error: ' . $response->get_error_message());
