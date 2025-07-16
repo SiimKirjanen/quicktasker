e338d46f425960535b6e026f649fe3abd1a55704
Siim Kirjanen <siim.kirjanen@pipedrive.com>
2025-07-16 13:38:25 +0300

    Turned off pro custom fields

--- build/app.asset.php
+++ build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '78ba0814bc8beafbfe44');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '1dacc4c65ed567e245dc');
--- build/userApp.asset.php
+++ build/userApp.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '5fb81f36127160ed4af2');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '5ce62f05f4aa7bf3b580');
