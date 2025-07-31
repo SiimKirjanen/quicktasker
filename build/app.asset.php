5b0914cefaee4872c580beac5c35a8722283b0ac
Siim Kirjanen <siim.kirjanen@pipedrive.com>
2025-07-31 14:11:30 +0300

    Free version fix

--- build/app.asset.php
+++ build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '18e9f7b0a42e58b21d7c');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '89233fbc0013e8c272a2');
