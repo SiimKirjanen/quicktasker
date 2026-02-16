4d28501db5245f2b63255ad63274930a576241ac
Siim Kirjanen <siim.kirjanen@gmail.com>
2026-02-16 17:29:39 +0200

    Audit fix, updated mjml to alpha 11, regenerated emails

--- build/app.asset.php
+++ build/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'efa4ce5964a05ce393a3');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '3b07c1c20c682657ecd6');
--- build/vendors.asset.php
+++ build/vendors.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '8309a13ef5bea868423f');
+<?php return array('dependencies' => array(), 'version' => 'e576438a125292d7e7f2');
