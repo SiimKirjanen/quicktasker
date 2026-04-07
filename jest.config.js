b68ee2e1ff8509905529a3d5462362e90cd61397
Siim Kirjanen <siim.kirjanen@gmail.com>
2026-04-07 19:37:19 +0300

    configure Jest to mock image imports

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "\\.(png|jpg|jpeg|gif|svg|webp)$": "<rootDir>/src/test-utils/__mocks__/fileMock.js",
   },
   testPathIgnorePatterns: ["/node_modules/", "/build/"],
   testMatch: ["<rootDir>/src/**/*.(test|spec).(ts|tsx)"],
