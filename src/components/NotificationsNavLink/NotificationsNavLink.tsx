a91a9ee50ef4a377b569bcfa01e1af25eb9b6963
Siim Kirjanen <siim.kirjanen@gmail.com>
2026-05-08 20:09:42 +0300

    Fix infinite render loop by flattening NotificationsNavLink layout

--- src/components/NotificationsNavLink/NotificationsNavLink.tsx
+++ src/components/NotificationsNavLink/NotificationsNavLink.tsx
@@ -1,6 +1,6 @@
-import { BellIcon } from "@heroicons/react/24/outline";
 import { useContext, useEffect } from "@wordpress/element";
 import { __ } from "@wordpress/i18n";
+import { IoNotificationsOutline } from "react-icons/io5";
 import { OPEN_NOTIFICATIONS_MODAL } from "../../constants";
 import { ModalContext } from "../../providers/ModalContextProvider";
 import { NotificationsContext } from "../../providers/NotificationsContextProvider";
@@ -25,24 +25,22 @@ function NotificationsNavLink({ pipelineId }: Props) {
 
   return (
     <div
-      className="wpqt-flex wpqt-items-center wpqt-cursor-pointer wpqt-gap-2 wpqt-group"
+      className="wpqt-relative wpqt-flex wpqt-items-center wpqt-cursor-pointer wpqt-gap-2 wpqt-group"
       data-testid="notifications-nav-link"
       onClick={() => modalDispatch({ type: OPEN_NOTIFICATIONS_MODAL })}
     >
-      <div className="wpqt-relative">
-        <BellIcon className="wpqt-size-5 wpqt-text-blue-400 group-hover:wpqt-text-blue-600" />
-        {unreadCount > 0 && (
-          <span
-            data-testid="notifications-unread-badge"
-            className="wpqt-absolute -wpqt-right-2 -wpqt-top-2 wpqt-flex wpqt-h-4 wpqt-min-w-4 wpqt-items-center wpqt-justify-center wpqt-rounded-full wpqt-bg-red-600 wpqt-px-1 wpqt-text-[10px] wpqt-font-semibold wpqt-text-white"
-          >
-            {unreadCount}
-          </span>
-        )}
-      </div>
+      <IoNotificationsOutline className="wpqt-size-5 wpqt-text-blue-400 group-hover:wpqt-text-blue-600" />
       <span className="wpqt-text-sm wpqt-blue-text group-hover:wpqt-text-blue-600">
         {__("Notifications", "quicktasker")}
       </span>
+      {unreadCount > 0 && (
+        <span
+          data-testid="notifications-unread-badge"
+          className="wpqt-absolute wpqt-left-3 -wpqt-top-3 wpqt-flex wpqt-h-4 wpqt-min-w-4 wpqt-items-center wpqt-justify-center wpqt-rounded-full wpqt-bg-red-400 wpqt-px-1 wpqt-text-[10px] wpqt-font-semibold wpqt-text-white"
+        >
+          {unreadCount}
+        </span>
+      )}
     </div>
   );
 }
